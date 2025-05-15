4099fa119bbff70fb03b1f595eb5b9f28ba45bcb
Ilaria <maffiolailaria1@gmail.com>
2025-05-15 15:15:12 +0200

    [UPD]Add react-router and react-router-dom dependencies; wrap App component with BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
-
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
+      <BrowserRouter>
       
+      </BrowserRouter>
     </>
   )
 }
