c3ba3cc931d37f9ec041aed7203e0b09da5f05a1
Ilaria <maffiolailaria1@gmail.com>
2025-05-16 15:55:52 +0200

    [UPD] Style adjustments in DetailProduct component for improved layout and readability

--- src/pages/products/DetailProduct.jsx
+++ src/pages/products/DetailProduct.jsx
@@ -16,7 +16,7 @@ const DetailProduct = () => {
     }, [])
   return (
     <div>
-        <h1>Dettaglio prodotto</h1>
+        <h1 className='text-center mb-3'>Dettaglio prodotto</h1>
         <div className="container">
             <div className="row">
                 <div className="col-12">
@@ -25,7 +25,7 @@ const DetailProduct = () => {
                             <img className='img-fluid' src={product.image} alt={product.title} />
                         </div>
                         <div className="card-body">
-                            <h6 className="card-title">{product.title}</h6>
+                            <h5 className="card-title">{product.title}</h5>
                             <p className="card-text">{product.category}</p>
                             <p className="card-text">€{product.price}</p>
                             <p className="card-text">{product.description}</p>
