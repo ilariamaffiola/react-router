387ea47eb92b7b2da5a07853d3bcd9c90418013d
Ilaria <maffiolailaria1@gmail.com>
2025-05-16 16:54:59 +0200

    [UPD]Refactor MainNavbar component by removing commented-out code and improve styling in index.css for better layout and readability

--- src/components/MainNavbar.jsx
+++ src/components/MainNavbar.jsx
@@ -16,6 +16,20 @@ function MainNavbar() {
     ]
   return (
     <header>
+      {/* <ul class="nav">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Active</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+  </li>
+</ul> */}
         <nav>
           <ul>
               {links.map((link, index) => (
